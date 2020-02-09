917a2296c577b5bcc19001d4d7f3c7c85102d8c3
omv0102 <omv98@outlook.com>
2020-02-10 00:09:09 +0700

    добавил файлы с  данными из старой проги

--- infbez1/Form1.cs
+++ infbez1/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//TODO сделать глобальную переменную под путь для всех файлов
+
 namespace LR1__program_CS
 {
     public partial class Form1 : Form
